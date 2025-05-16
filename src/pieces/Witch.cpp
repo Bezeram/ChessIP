df3e5102cc5bb524949d2f0691660bece7c810d9
graur <graurgdanmihai@gmail.com>
2025-05-17 01:00:28 +0300

    tweaked witch

--- src/pieces/Witch.cpp
+++ src/pieces/Witch.cpp
@@ -22,10 +22,13 @@ void Witch::GetLegalMoves(sf::Vector2i piecePosition, std::vector<ActionMove>& l
 						const auto& targetPiece = boardMatrix[targetSquare.y][targetSquare.x];
 						if (targetPiece.get() == nullptr)
 						{
-							// Check if the target square is not occupied by a friendly piece
+							legalMoves.push_back(ActionMove(targetSquare, MoveType::Move));
+						}
+						else
+						{
 							if (!m_Board->IsTargetFriendly(PieceMove(piecePosition, targetSquare)))
 							{
-								legalMoves.push_back(ActionMove(targetSquare));
+								legalMoves.push_back(ActionMove(targetSquare, MoveType::Action));
 							}
 						}
 					}
@@ -43,10 +46,13 @@ void Witch::GetLegalMoves(sf::Vector2i piecePosition, std::vector<ActionMove>& l
 						const auto& targetPiece = boardMatrix[targetSquare.y][targetSquare.x];
 						if (targetPiece.get() == nullptr)
 						{
-							// Check if the target square is not occupied by a friendly piece
+							legalMoves.push_back(ActionMove(targetSquare, MoveType::Move));
+						}
+						else
+						{
 							if (!m_Board->IsTargetFriendly(PieceMove(piecePosition, targetSquare)))
 							{
-								legalMoves.push_back(ActionMove(targetSquare));
+								legalMoves.push_back(ActionMove(targetSquare, MoveType::Action));
 							}
 						}
 					}
@@ -57,57 +63,64 @@ void Witch::GetLegalMoves(sf::Vector2i piecePosition, std::vector<ActionMove>& l
 
 void Witch::ExecuteMove(BoardMatrix& board, PiecePosition piecePosition, ActionMove move)
 {
-	// Not empty square, so there is an enemy on that square
-	if (board[move.TargetSquare.x][move.TargetSquare.y] != nullptr)
+	// Normal move
+	if (move.MoveType == MoveType::Move)
 	{
-		// Capture move
-		PiecePosition targetSquare = move.TargetSquare;
-		// Stun enemy in the square
-		board[targetSquare.y][targetSquare.x]->AddEffect(Effect::Stun);
-
-		if (Witch::m_UpgradeLevel == 3)
-		{
-			// For level 3, hex enemies with same x value
-			for (int dx = 0; dx < m_Board->GetSize(); dx += 1)
-			{
-				PiecePosition hexSquare = sf::Vector2i(targetSquare.y, dx);
-				if (IsCellInBounds(hexSquare, m_Board->GetSize()))
-				{
-					const auto& targetPiece = board[hexSquare.y][hexSquare.x];
-					if (targetPiece.get() != nullptr && targetPiece->GetPieceColor() != m_Color)
-					{
-						targetPiece->AddEffect(Effect::Hex);
-					}
-				}
-			}
-		}
-		else 
-		{
-			// For level 1 and 2, hex enemies in two squares to left and to right
-			for (int dx = -2; dx <= 2; dx += 1)
-			{
-				PiecePosition hexSquare = targetSquare + sf::Vector2i(dx, 0);
-				if (IsCellInBounds(hexSquare, m_Board->GetSize()))
-				{
-					const auto& targetPiece = board[hexSquare.y][hexSquare.x];
-					if (targetPiece.get() != nullptr && targetPiece->GetPieceColor() != m_Color)
-					{
-						targetPiece->AddEffect(Effect::Hex);
-					}
-				}
-			}
-		}
-
-	}
-	else
-	{
-		// Normal move
 		PiecePosition targetSquare = move.TargetSquare;
 		// Move piece to the new square
 		board[targetSquare.y][targetSquare.x] = std::move(board[piecePosition.y][piecePosition.x]);
 		// Clear old position
 		board[piecePosition.y][piecePosition.x] = nullptr;
 	}
+	else
+	// Action move
+	{
+		std::cout << "ACTION MOVE!" << std::endl;
+		if (board[move.TargetSquare.y][move.TargetSquare.x] != nullptr)
+		{
+			PiecePosition targetSquare = move.TargetSquare;
+			// Stun enemy in the square
+			board[targetSquare.y][targetSquare.x]->AddEffect(Effect::Stun, 1);
+			std::cout << "STUNNED!" << std::endl;
+
+			if (Witch::m_UpgradeLevel == 3)
+			{
+				// For level 3, hex enemies with same x value
+				for (int dx = 0; dx < m_Board->GetSize(); dx += 1)
+				{
+					PiecePosition hexSquare = sf::Vector2i(dx, targetSquare.y);
+					if (IsCellInBounds(hexSquare, m_Board->GetSize()))
+					{
+						const auto& targetPiece = board[hexSquare.y][hexSquare.x];
+						if (!m_Board->IsTargetFriendly(PieceMove(piecePosition, hexSquare))
+							&& targetPiece != nullptr)
+						{
+							targetPiece->AddEffect(Effect::Hex, 1);
+						}
+					}
+				}
+			}
+			else
+			{
+				// For level 1 and 2, hex enemies in two squares to left and to right
+				for (int dx = -2; dx <= 2; dx += 1)
+				{
+					PiecePosition hexSquare = targetSquare + sf::Vector2i(dx, 0);
+					if (IsCellInBounds(hexSquare, m_Board->GetSize()))
+					{
+						const auto& targetPiece = board[hexSquare.y][hexSquare.x];
+						if (!m_Board->IsTargetFriendly(PieceMove(piecePosition, hexSquare))
+							&& targetPiece != nullptr)
+						{
+							targetPiece->AddEffect(Effect::Hex, 1);
+						}
+					}
+				}
+			}
+
+		}
+	}
+	
 }
 
 PieceType Witch::GetPieceType()
