464a246f0a7c2ea6420476fb16d109603509cacc
graurgg <117681383+graurgg@users.noreply.github.com>
2025-05-28 13:01:12 +0300

    added builder

--- src/pieces/Wall.h
+++ src/pieces/Wall.h
@@ -3,13 +3,13 @@
 #include "BasePiece.h"
 #include "../Utils.h"
 
-class WhiteBuilder : public BasePiece
+class Wall : public BasePiece
 {
 public:
+	Wall(std::shared_ptr<Board> board, PieceColor color, uint32_t upgradeLevel = 1);
 	void GetLegalMoves(sf::Vector2i piecePosition, std::vector<ActionMove>& legalMoves) override;
 	void GetRange(PiecePosition piecePosition, std::vector<ActionMove>& legalMoves) override;
 	PieceType GetPieceType();
 
 private:
-	// TODO: Implement ExecuteMove() if necessary
 };
